cddcbf297c993a6c25d396af1a2530a1908de6d1
Umesh Poswal <umesh.poswal@telusinternational.com>
2021-08-24 19:01:39 +0900

    added functions to get spread and delta

--- binance/lib.py
+++ binance/lib.py
@@ -1,8 +1,10 @@
 import binance
 from binance.spot import Spot
 import datetime
-import config
 import logging
+import time
+
+import config
 
 _client = None
 
@@ -49,17 +51,17 @@ def get_sym_for_quote_asset(quote_asset="BTC"):
 def highest_trades_per_symbol(quote_asset="USDT", by="count", limit=5):
     if by not in ["count", "volume"]:
         logging.error("highest_trades_per_symbol - can be aggregated based on count and volume only")
-        return {}
+        return None
     symbols = get_sym_for_quote_asset(quote_asset)
     trades_per_symbol = {}
     for symbol in symbols:
         one_day_ticker, _err = _spot_api_call(_get_client().ticker_24hr, symbol=symbol)
         if _err is not None:
             logging.error(f"Got error while highest_trades_per_symbol for {symbol}", _err)
-            return {}
+            return None
         trades_per_symbol[symbol] = float(one_day_ticker[by])
     sorted_trades = sorted(trades_per_symbol.items(), key=lambda item: item[1], reverse=True)
-    return sorted_trades[:limit], None
+    return sorted_trades[:limit]
 
 
 def get_notaional_value_of_bids_and_asks(symbols, limit=200):
@@ -84,3 +86,24 @@ def get_price_spread(symbols):
             return {}
         spread_per_symbol[symbol] = float(book_ticker["bidPrice"]) - float(book_ticker["askPrice"])
     return spread_per_symbol
+
+def get_spread_and_delta(curr_spread_per_sym, prev_spread_per_sym):
+    res = {}
+    if not prev_spread_per_sym:
+        prev_spread_per_sym = curr_spread_per_sym
+    for symbol in curr_spread_per_sym:
+        delta = curr_spread_per_sym[symbol] - prev_spread_per_sym[symbol]
+        if delta < 0:
+            delta = -1 * delta
+        res[symbol] = {"s": curr_spread_per_sym[symbol], "d": delta}
+    return res
+
+
+def get_periodic_spread(symbols, period=10):
+    starttime = time.time()
+    prev_spread, curr_spread = None, None
+    while True:
+        prev_spread = curr_spread
+        curr_spread = get_price_spread(symbols)
+        yield get_spread_and_delta(curr_spread, prev_spread)
+        time.sleep(period - ((time.time() - starttime) % period))
